e47e91d7ad037ccc773d0b2dcf187cbfa1fcce72
alonsokratos31 <alonso.garcia025@gmail.com>
2024-04-19 22:53:53 -0400

    Se actualizo el main

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,7 @@ async function bootstrap() {
   app.useGlobalFilters(new RpcCustomExceptionFilter());
   await app.listen(envs.port);
   console.log('Hola mundo - primer cambio');
+  console.log('Hola mundo - segundo cambio');
   logger.log(`Gateway running on port ${envs.port}`);
 }
 bootstrap();
