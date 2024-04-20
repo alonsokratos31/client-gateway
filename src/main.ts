ece9acea22e92406d4b72ebeb450ff511d15c76b
alonsokratos31 <alonso.garcia025@gmail.com>
2024-04-19 22:43:29 -0400

    Add console log statement - Hola Mundo - Primer cambio

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,7 @@ async function bootstrap() {
   );
   app.useGlobalFilters(new RpcCustomExceptionFilter());
   await app.listen(envs.port);
+  console.log('Hola mundo - primer cambio');
   logger.log(`Gateway running on port ${envs.port}`);
 }
 bootstrap();
